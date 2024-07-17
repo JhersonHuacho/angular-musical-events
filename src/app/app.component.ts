2947cfb66819395346f430e60a7406390292a671
Francisco <jhersonhuacho@gmail.com>
2024-07-17 10:05:29 -0500

    implement basic route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    EventDetailComponent,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
