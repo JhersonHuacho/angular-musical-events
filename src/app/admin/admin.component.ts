e288ef54d23bca13cec9bc358d631d5a8cccbdc3
Francisco <jhersonhuacho@gmail.com>
2024-08-29 18:43:10 -0500

    componente admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../shared/components/footer/footer.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { LoggedInHeaderComponent } from '../shared/components/logged-in-header/logged-in-header.component';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [
+    LoggedInHeaderComponent,
+    FooterComponent,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
