c5da4de68034af13c6ba257ff70600590098a3a5
Francisco <jhersonhuacho@gmail.com>
2024-08-30 11:57:58 -0500

    add  layout genre

--- src/app/admin/genres/genres.component.ts
+++ src/app/admin/genres/genres.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {  FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs';
+
+interface Status {
+  value: boolean;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-genres',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatTabsModule
+  ],
   templateUrl: './genres.component.html',
   styleUrl: './genres.component.css'
 })
 export class GenresComponent {
+  formBuilder = inject(FormBuilder);
 
+  genresForm = this.formBuilder.group({
+    description: [''],
+    state: ['']
+  })
+
+  status: Status[] = [
+    {value: true, viewValue: 'ACTIVO'},
+    {value: false, viewValue: 'INACTIVO'}
+  ];
+
+  onSubmit() {
+    console.warn(this.genresForm.value);
+  }
 }
