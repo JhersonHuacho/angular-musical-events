f7af19b4009985cec55dce8b27f1589bd3777231
Francisco <jhersonhuacho@gmail.com>
2024-09-05 22:27:33 -0500

    maquetación sales

--- src/app/customer/my-purchases/my-purchases.component.ts
+++ src/app/customer/my-purchases/my-purchases.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 @Component({
   selector: 'app-my-purchases',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatTableModule,MatPaginatorModule],
   templateUrl: './my-purchases.component.html',
   styleUrl: './my-purchases.component.css'
 })
 export class MyPurchasesComponent {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+];
